13231fed7be344e0f0439e40c9e9ce013d5c00ce
Aleksei Chaika <chaikaam1986@gmail.com>
2023-05-30 16:41:54 +0300

    Initial project with slow 3rd party server server

---
+++ slow-server/src/main/java/pro/seag/SlowServer.java
@@ -0,0 +1,13 @@
+package pro.seag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowServer.class, args);
+    }
+
+}
